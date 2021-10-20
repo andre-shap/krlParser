e1ce433222858253bc5c3c12294761442b2fcc86
Andrey Shaposhnikov <68745926+andre-shap@users.noreply.github.com>
2021-10-20 12:07:19 +0300

    Add files via upload

--- directLinematics/main.py
+++ directLinematics/main.py
@@ -1,19 +1,25 @@
 import math
 import numpy as np
 
+
+
 print("Введите обобщенные координаты манипулятора")
-'''
+print('Введите a1: ')
 a1 = float(input())
+print('Введите a2: ')
 a2 = float(input())
+print('Введите a3: ')
 a3 = float(input())
+print('Введите a4: ')
 a4 = float(input())
+print('Введите a5: ')
 a5 = float(input())
-'''
-a1=0
-a2=0
-a3=0
-a4=0
-a5=90
+
+# a1=0
+# a2=0
+# a3=0
+# a4=0
+# a5=0
 # Параметры Денавита-Хартенберга
 # Если считаем относительно стрелы
 
@@ -38,8 +44,9 @@ rzList=[]
 #Перемещение относительно осей z (зависят от d)
 diList = []
 #Перемещение относительно осей x (зависят от a)
-
 axList = []
+ryList = []
+
 # Памятка: 'a1...a5' - обобщенные координаты, 'a' - один из параметров Денавита-Хартенберга
 # Заполнение списков матриц, описывающие вклады каждого параметра Денавита-Хартенберга
 for i in range (0,5):
@@ -66,14 +73,14 @@ for i in range (0,5):
         h=np.matmul(rzdiai, rxList[i])
         #print('Итоговая матрица:',h)
         hList.append(h)
-print('Список итоговых матриц: ', hList)
+#print('Список итоговых матриц: ', hList)
 # Единичная матрица
 unitMatrix=np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 # Перемножение матриц преобразования каждого звена - получение общей матрицы преобразования
 hTotal=np.matmul(unitMatrix, hList[0])
 for i in range (1,5):
     hTotal=np.matmul(hTotal, hList[i])
-print('Итоговая матрица преобразований:', hTotal)
+#print('Итоговая матрица преобразований:', hTotal)
 # Список для хранения декартовых координат (x,y,z) общей матрицы преобразования
 p = []
 for i in range (0,3):
@@ -119,34 +126,51 @@ Rzyx=np.array([[math.cos(alp)*math.cos(bet), -math.sin(alp)*math.cos(gam)+math.c
 '''
 
 if r31!=1 and r31!=-1:
-    bettaAngle=round(math.degrees(-math.atan2(r31, math.sqrt(1-math.pow(r31,2)))), 2)
-    gammaAngle=round(math.degrees(math.atan2(r21, r11)), 2)
-    alphaAngle=round(math.degrees(math.atan2(r32, r33)), 2)
-    print('Углы Тейта-Брайана(ZYX) (r31!=+-1): ', gammaAngle, bettaAngle, alphaAngle)
+    bettaAngle=round(math.degrees(math.asin(-r31)), 2)
+    #bettaAngle=round(math.degrees(-math.atan2(r31, math.sqrt(1-math.pow(r31,2)))), 2)
+    alphaAngle=round(math.degrees(math.atan2(r21, r11)), 2)
+    gammaAngle=round(math.degrees(math.atan2(r32, r33)), 2)
+    print('Углы Тейта-Брайана(ZYX) (r31!=+-1): ', alphaAngle, bettaAngle, gammaAngle)
 elif r31==-1:
-    #В таком случае можно использовать один из 2 способов поиска суммы gamma и alpha
-    sumGammaAlpha=round(math.degrees(math.atan2(r23, r22)), 2)
-    #sumGammaAlpha=round(math.degrees(math.atan2(r12, r13)), 2)
-    print('Сумма альфа и гамма (r31=-1): ', sumGammaAlpha)
-    # Из-за того, что мы на данном этапе можно получить только сумму альфа и гамма
-    # нужно принять один из этих углов за некую величину (например, 0) и получить вторую
-    alphaAngle=0
-    gammaAngle=sumGammaAlpha-alphaAngle
-    bettaAngle=round(math.degrees(math.asin(-r31)))
-    print('Углы Тейта-Брайана(ZYX) (r31!=+1): ', gammaAngle, bettaAngle, alphaAngle)
-    gammaAngle = 0
-    alphaAngle = sumGammaAlpha - gammaAngle
-    print('Углы Тейта-Брайана(ZYX) (r31!=+1): ', gammaAngle, bettaAngle, alphaAngle)
-elif r31==1:
-    difGammaAlpha=round(math.degrees(-math.atan2(r12, r13)), 2)
-    #difGammaAlpha=round(math.degrees(-math.atan2(r23, r22), 2)
-    print('Разность гамма и альфа (r31=1): ', difGammaAlpha)
+    #В таком случае можно использовать один из 2 способов поиска разности gamma и alpha
+    difGammaAlpha=round(math.degrees(math.atan2(r12, r22)), 2)
+    bettaAngle = round(math.degrees(math.asin(-r31)), 2)
+    #bettaAngle = round(math.degrees(-math.atan2(r31, math.sqrt(1 - math.pow(r31, 2)))), 2)
+    print('Разность гамма и альфа (r31==-1): ', difGammaAlpha)
     # Из-за того, что мы на данном этапе можно получить только разность альфа и гамма
     # нужно принять один из этих углов за некую величину (например, 0) и получить вторую
-    bettaAngle = round(math.degrees(math.asin(-r31)))
-    alphaAngle = 0
-    gammaAngle = difGammaAlpha + alphaAngle
-    print('Углы Тейта-Брайана(ZYX) (r31!=+1): ', gammaAngle, bettaAngle, alphaAngle)
+
+    #print('Можно посчитать только разность гамма и альфа. Введите альфа: ')
+    #alphaAngle=float(input())
+    alphaAngle=0
+    gammaAngle=difGammaAlpha+alphaAngle
+    print('Углы Тейта-Брайана(ZYX) (r31==-1) (alphaAngle='+str(alphaAngle)+'): ', alphaAngle, bettaAngle, gammaAngle)
+    # print('Можно посчитать только разность гамма и альфа. Введите гамма: ')
+    # gammaAngle=float(input())
     gammaAngle = 0
-    alphaAngle = difGammaAlpha + gammaAngle
-    print('Углы Тейта-Брайана(ZYX) (r31!=+1): ', gammaAngle, bettaAngle, alphaAngle)
+    alphaAngle = difGammaAlpha - gammaAngle
+    print('Углы Тейта-Брайана(ZYX) (r31==-1) (gammaAngle='+str(gammaAngle)+'): ', alphaAngle, bettaAngle, gammaAngle)
+elif r31==1:
+    sumGammaAlpha=round(math.degrees(math.atan2(r12, r13)), 2)
+    print('Сумма гамма и альфа (r31==1): ', sumGammaAlpha)
+    # Из-за того, что мы на данном этапе можно получить только сумму альфа и гамма
+    # нужно принять один из этих углов за некую величину (например, 0) и получить вторую
+    bettaAngle = round(math.degrees(math.asin(-r31)), 2)
+    #bettaAngle = round(math.degrees(-math.atan2(r31, math.sqrt(1 - math.pow(r31, 2)))), 2)
+
+    # print('Можно посчитать только сумму гамма и альфа. Введите альфа: ')
+    # alphaAngle=float(input())
+    alphaAngle = 0
+    gammaAngle = sumGammaAlpha - alphaAngle
+    print('Углы Тейта-Брайана(ZYX) (r31==1): ', alphaAngle, bettaAngle, gammaAngle)
+
+    # print('Можно посчитать только сумму гамма и альфа. Введите гамма: ')
+    # gammaAngle=float(input())
+    gammaAngle = 0
+    alphaAngle = sumGammaAlpha - gammaAngle
+    print('Углы Тейта-Брайана(ZYX) (r31==1): ', alphaAngle, bettaAngle, gammaAngle)
+
+
+
+
+
